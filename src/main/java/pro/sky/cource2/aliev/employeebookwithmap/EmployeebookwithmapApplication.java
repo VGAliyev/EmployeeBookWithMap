739aaa7dc17ce74d802e3de783caf7b47bf19267
VGAliyev <v.aliev84@yandex.ru>
2023-11-06 16:12:26 +0400

    Initial commit

---
+++ src/main/java/pro/sky/cource2/aliev/employeebookwithmap/EmployeebookwithmapApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.cource2.aliev.employeebookwithmap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeebookwithmapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeebookwithmapApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pro/sky/cource2/aliev/employeebookwithmap/EmployeebookwithmapApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.cource2.aliev.employeebookwithmap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeebookwithmapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
